b993270948498611685ca50efb9db1325ef4fe1b
Aurélien Calonne <aurel@nexedi.com>
2007-01-31 16:39:40 +0000

    explicitely convert port number to int
    git-svn-id: https://svn.erp5.org/repos/neo/branches/prototype3@103 71dcc9de-d417-0410-9af5-da40c76e7ee4

--- neo/client/config.py
+++ neo/client/config.py
@@ -4,6 +4,6 @@ class NEOStorage(BaseConfig):
 
   def open(self):
     from NEOStorage import NEOStorage
-    return NEOStorage(master_addr = self.config.master_addr, master_port = self.config.master_port, name = self.config.name)
+    return NEOStorage(master_addr = self.config.master_addr, master_port = int(self.config.master_port), name = self.config.name)
 
 
