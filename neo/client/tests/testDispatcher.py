9e4d305ab5c11be9232d228c3a90af00d440204b
Vincent Pelletier <vincent@nexedi.com>
2009-04-02 05:10:52 +0000

    Remove unused import.
    git-svn-id: https://svn.erp5.org/repos/neo/branches/prototype3@324 71dcc9de-d417-0410-9af5-da40c76e7ee4

--- neo/client/tests/testDispatcher.py
+++ neo/client/tests/testDispatcher.py
@@ -16,7 +16,6 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 import unittest
-from mock import Mock, ReturnValues
 
 from neo.client.dispatcher import Dispatcher
 
