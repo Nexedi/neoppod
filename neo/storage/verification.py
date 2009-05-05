5722740052af6163cf26eaebf257a0b2afd083ea
Grégory Wisniewski <gregory@nexedi.com>
2009-05-05 13:42:46 +0000

    Bug fix in storage/verification.py, a line was missing.
    git-svn-id: https://svn.erp5.org/repos/neo/branches/prototype3@423 71dcc9de-d417-0410-9af5-da40c76e7ee4

--- neo/storage/verification.py
+++ neo/storage/verification.py
@@ -100,6 +100,7 @@ class VerificationEventHandler(StorageEventHandler):
             p = Packet()
             p.acceptNodeIdentification(packet.getId(), STORAGE_NODE_TYPE,
                                        app.uuid, app.server[0], app.server[1],
+                                       app.num_partitions, app.num_replicas,
                                        uuid)
             conn.addPacket(p)
 
