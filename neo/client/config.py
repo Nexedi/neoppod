8f321c6c960cd1af2c237556fec166d37021f63e
Vincent Pelletier <vincent@nexedi.com>
2009-07-20 19:18:44 +0000

    Reindent to 4 spaces.
    git-svn-id: https://svn.erp5.org/repos/neo/branches/prototype3@895 71dcc9de-d417-0410-9af5-da40c76e7ee4

--- neo/client/config.py
+++ neo/client/config.py
@@ -19,9 +19,9 @@ from ZODB.config import BaseConfig
 
 class NeoStorage(BaseConfig):
 
-  def open(self):
-    from Storage import Storage
-    return Storage(master_nodes = self.config.master_nodes, name = self.config.name,
-                   connector = self.config.connector)
+    def open(self):
+        from Storage import Storage
+        return Storage(master_nodes = self.config.master_nodes, name = self.config.name,
+                       connector = self.config.connector)
 
 
