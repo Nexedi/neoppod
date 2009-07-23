88dafac587448b337c63dfad5c5860f456e49877
Grégory Wisniewski <gregory@nexedi.com>
2009-07-23 12:43:59 +0000

    Don't import test cases in module's __init__ to avoid crash all tests with an import error.
    git-svn-id: https://svn.erp5.org/repos/neo/branches/prototype3@978 71dcc9de-d417-0410-9af5-da40c76e7ee4

--- neo/tests/client/__init__.py
+++ neo/tests/client/__init__.py
@@ -1,11 +1,1 @@
-from neo.client.tests.testClientApp import ClientApplicationTests
-from neo.client.tests.testClientHandler import ClientHandlerTests
-from neo.client.tests.testConnectionPool import ConnectionPoolTests
-from neo.client.tests.testDispatcher import DispatcherTests
 
-__all__ = [
-    'ClientApplicationTests', 
-    'ClientHandlerTests', 
-    'ConnectionPoolTests',
-    'DispatcherTests',
-]
--- neo/tests/storage/__init__.py
+++ neo/tests/storage/__init__.py
@@ -1,18 +1,1 @@
-from neo.storage.tests.testStorageApp import StorageAppTests
-from neo.storage.tests.testStorageMySQLdb import StorageMySQSLdbTests 
-from neo.storage.tests.testInitializationHandler import StorageInitializationHandlerTests
-from neo.storage.tests.testVerificationHandler import StorageVerificationHandlerTests 
-from neo.storage.tests.testStorageHandler import StorageStorageHandlerTests
-from neo.storage.tests.testClientHandler import StorageClientHandlerTests
-from neo.storage.tests.testMasterHandler import StorageMasterHandlerTests
 
-
-__all__ = [
-    'StorageAppTests',
-    'StorageMySQSLdbTests',
-    'StorageInitializationHandlerTests',
-    'StorageClientHandlerTests',
-    'StorageMasterHandlerTests',
-    'StorageStorageHandlerTests',
-    'StorageVerificationHandlerTests',
-]
