60863842c44fb1962cbb29bb637913cb60479369
Grégory Wisniewski <gregory@nexedi.com>
2009-07-27 12:13:12 +0000

    Fix typo.
    git-svn-id: https://svn.erp5.org/repos/neo/branches/prototype3@1016 71dcc9de-d417-0410-9af5-da40c76e7ee4

--- neo/client/iterator.py
+++ neo/client/iterator.py
@@ -16,7 +16,7 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 from ZODB import BaseStorage
-from neo import util import
+from neo import util
 
 
 class Record(BaseStorage.DataRecord):
