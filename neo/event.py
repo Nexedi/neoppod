bd2a61713ab531a0d6b148f23a1cf81f6f84a978
Vincent Pelletier <vincent@nexedi.com>
2009-08-06 13:14:22 +0000

    Even if a handler raises, the connection might still contain pending packets. So it should not prevent appending the connection to the list.
    git-svn-id: https://svn.erp5.org/repos/neo/branches/prototype3@1221 71dcc9de-d417-0410-9af5-da40c76e7ee4

--- neo/event.py
+++ neo/event.py
@@ -251,11 +251,13 @@ class EpollEventManager(object):
             # See if there is anything to process
             to_process = self._getPendingConnection()
         if to_process is not None:
-            # Process
-            to_process.process()
-            # ...and requeue if there are pending messages
-            if to_process.hasPendingMessages():
-                self._addPendingConnection(to_process)
+            try:
+                # Process
+                to_process.process()
+            finally:
+                # ...and requeue if there are pending messages
+                if to_process.hasPendingMessages():
+                    self._addPendingConnection(to_process)
 
     def _poll(self, timeout = 1):
         rlist, wlist = self.epoll.poll(timeout)
