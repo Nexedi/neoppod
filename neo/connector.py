60fc3f826e6c52439415bf315d8083aa58c75d02
Grégory Wisniewski <gregory@nexedi.com>
2009-09-21 13:15:08 +0000

    Improve error log message.
    git-svn-id: https://svn.erp5.org/repos/neo/trunk@1288 71dcc9de-d417-0410-9af5-da40c76e7ee4

--- neo/connector.py
+++ neo/connector.py
@@ -70,8 +70,8 @@ class SocketConnector:
                     raise ConnectorInProgressException
                 if err == errno.ECONNREFUSED:
                     raise ConnectorConnectionRefusedException
-                raise ConnectorException, 'makeClientConnection failed: ' \
-                    '%s:%s' % (err, errmsg)
+                raise ConnectorException, 'makeClientConnection to %s failed: ' \
+                    '%s:%s' % (addr, err, errmsg)
         finally:
             logging.debug('%r connecting to %r', self.socket.getsockname(),
                           addr)
@@ -85,8 +85,8 @@ class SocketConnector:
             self.socket.listen(5)
         except socket.error, (err, errmsg):
             self.socket.close()
-            raise ConnectorException, 'makeListeningConnection failed: %s:%s' % \
-                (err, errmsg)
+            raise ConnectorException, 'makeListeningConnection on %s failed: %s:%s' % \
+                (addr, err, errmsg)
 
     def getError(self):
         return self.socket.getsockopt(socket.SOL_SOCKET, socket.SO_ERROR)
