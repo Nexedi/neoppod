0aba041881de17a59d5968463977e78607c76279
Grégory Wisniewski <gregory@nexedi.com>
2009-10-07 20:06:18 +0000

    Delete a space from file headers to fit in 80 columns.
    git-svn-id: https://svn.erp5.org/repos/neo/trunk@1362 71dcc9de-d417-0410-9af5-da40c76e7ee4

--- neo/protocol.py
+++ neo/protocol.py
@@ -13,7 +13,7 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 
 from struct import pack, unpack, error
 from socket import inet_ntoa, inet_aton
