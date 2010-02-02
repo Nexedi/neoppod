1c8cc1fb997b4bd69deb5285ab281e57a9b581ec
Grégory Wisniewski <gregory@nexedi.com>
2010-02-02 10:16:44 +0000

    Remove unused import.
    git-svn-id: https://svn.erp5.org/repos/neo/trunk@1585 71dcc9de-d417-0410-9af5-da40c76e7ee4

--- neo/client/handlers/storage.py
+++ neo/client/handlers/storage.py
@@ -15,7 +15,6 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 
-from neo import logging
 from ZODB.TimeStamp import TimeStamp
 
 from neo.client.handlers import BaseHandler, AnswerBaseHandler
