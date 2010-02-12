5a810c6f5f39bd2b14b9595a6fe0f10fde7e108f
Vincent Pelletier <vincent@nexedi.com>
2010-02-12 22:07:32 +0000

    Disable attribute tracker by default.
    This increases performance noticeably - especially since it was enabled on
    connection, where properties change very often (such as data buffers).
    
    git-svn-id: https://svn.erp5.org/repos/neo/trunk@1753 71dcc9de-d417-0410-9af5-da40c76e7ee4

--- neo/attributeTracker.py
+++ neo/attributeTracker.py
@@ -15,7 +15,7 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 
-ATTRIBUTE_TRACKER_ENABLED = True
+ATTRIBUTE_TRACKER_ENABLED = False
 
 from neo.locking import LockUser
 
