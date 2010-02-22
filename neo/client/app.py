Nexedi/neoppod

commit 2b5dcd89e3b53da0a5b92990428e28d71fa991e3

Store uncompressed data if compressed data is bigger.
git-svn-id: https://svn.erp5.org/repos/neo/trunk@1820 71dcc9de-d417-0410-9af5-da40c76e7ee4

neo/client/app.py
@@ -556,8 +556,13 @@ class Application(object):
             # this is a George Bailey object, stored as an empty string
             data = ''
         compressed_data = compress(data)
+        if len(compressed_data) > data:
+            compressed_data = data
+            compression = 0
+        else:
+            compression = 1
         checksum = makeChecksum(compressed_data)
-        p = Packets.AskStoreObject(oid, serial, 1,
+        p = Packets.AskStoreObject(oid, serial, compression,
                  checksum, compressed_data, self.local_var.tid)
         # Store object in tmp cache
         self.local_var.data_dict[oid] = data

commit 84d4e083bc5ca7c5f0b5457999ec225f3c3c97ef

Fix bug introduced in r1820 causing data to be always stored uncompressed.
git-svn-id: https://svn.erp5.org/repos/neo/trunk@1824 71dcc9de-d417-0410-9af5-da40c76e7ee4

neo/client/app.py
@@ -556,7 +556,7 @@ class Application(object):
             # this is a George Bailey object, stored as an empty string
             data = ''
         compressed_data = compress(data)
-        if len(compressed_data) > data:
+        if len(compressed_data) > len(data):
             compressed_data = data
             compression = 0
         else: