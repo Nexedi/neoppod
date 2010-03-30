7bb627e8f45c96afddea52d9bd12de7ed78fa95a
Grégory Wisniewski <gregory@nexedi.com>
2010-03-30 12:17:09 +0000

    Merge Client and ImportExport functional tests.
    git-svn-id: https://svn.erp5.org/repos/neo/trunk@1986 71dcc9de-d417-0410-9af5-da40c76e7ee4

--- neo/tests/functional/testClient.py
+++ neo/tests/functional/testClient.py
@@ -15,13 +15,28 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 
+import os
 import unittest
 import transaction
 import ZODB
+from ZODB.FileStorage import FileStorage
 from ZODB.POSException import ConflictError
 from Persistence import Persistent
 
 from neo.tests.functional import NEOCluster, NEOFunctionalTest
+
+TREE_SIZE = 6
+
+class Tree(Persistent):
+    """ A simple binary tree """
+
+    def __init__(self, depth):
+        self.depth = depth
+        if depth <= 0:
+            return
+        depth -= 1
+        self.right = Tree(depth)
+        self.left = Tree(depth)
 
 
 # simple persitent object with conflict resolution
@@ -151,6 +166,66 @@ class ClientTests(NEOFunctionalTest):
         c2.cacheMinimize()
         self.assertEqual(c1.root()['item'], 1)
         self.assertEqual(c2.root()['item'], 0)
+
+    def __checkTree(self, tree, depth=TREE_SIZE):
+        self.assertTrue(isinstance(tree, Tree))
+        self.assertEqual(depth, tree.depth)
+        depth -= 1
+        if depth <= 0:
+            return
+        self.__checkTree(tree.right, depth)
+        self.__checkTree(tree.left, depth)
+
+    def __getDataFS(self, reset=False):
+        name = os.path.join(self.getTempDirectory(), 'data.fs')
+        if reset and os.path.exists(name):
+            os.remove(name)
+        storage = FileStorage(file_name=name)
+        db = ZODB.DB(storage=storage)
+        return (db, storage)
+
+    def __populate(self, db, tree_size=TREE_SIZE):
+        conn = db.open()
+        root = conn.root()
+        root['trees'] = Tree(tree_size)
+        transaction.commit()
+        conn.close()
+
+    def testImport(self):
+
+        # source database
+        dfs_db, dfs_storage  = self.__getDataFS()
+        self.__populate(dfs_db)
+
+        # create a neo storage
+        self.neo.start()
+        neo_storage = self.neo.getZODBStorage()
+
+        # copy data fs to neo
+        neo_storage.copyTransactionsFrom(dfs_storage, verbose=0)
+
+        # check neo content
+        (neo_db, neo_conn) = self.neo.getZODBConnection()
+        self.__checkTree(neo_conn.root()['trees'])
+
+    def testExport(self):
+
+        # create a neo storage
+        self.neo.start()
+        (neo_db, neo_conn) = self.neo.getZODBConnection()
+        self.__populate(neo_db)
+
+        # copy neo to data fs
+        dfs_db, dfs_storage  = self.__getDataFS(reset=True)
+        neo_storage = self.neo.getZODBStorage()
+        dfs_storage.copyTransactionsFrom(neo_storage, verbose=0)
+
+        # check data fs content
+        conn = dfs_db.open()
+        root = conn.root()
+
+        self.__checkTree(root['trees'])
+
 
 def test_suite():
     return unittest.makeSuite(ClientTests)
