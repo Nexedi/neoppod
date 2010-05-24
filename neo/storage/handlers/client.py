96be0c666bd9cdd56cbe9f162cf1bf9c7a7d082e
Grégory Wisniewski <gregory@nexedi.com>
2010-05-24 09:49:55 +0000

    Add missing import.
    git-svn-id: https://svn.erp5.org/repos/neo/trunk@2118 71dcc9de-d417-0410-9af5-da40c76e7ee4

--- neo/storage/handlers/client.py
+++ neo/storage/handlers/client.py
@@ -15,6 +15,7 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 
+from neo import logging
 from neo import protocol
 from neo.protocol import Packets, LockState
 from neo.storage.handlers import BaseClientAndStorageOperationHandler
