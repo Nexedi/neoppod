Nexedi/neoppod

commit 436e3a5cb88e41261ed5bf4be312261ddd78137b

Stop defining and using default "NEO" logger.
git-svn-id: https://svn.erp5.org/repos/neo/trunk@2371 71dcc9de-d417-0410-9af5-da40c76e7ee4

neo/__init__.py
@@ -17,8 +17,7 @@
 
 import logging as logging_std
 
-# default logger
-logging = logging_std.getLogger('NEO')
+# "logging" is available here only once setupLog has been called.
 
 PREFIX = '%(asctime)s %(levelname)-9s %(name)-10s'
 SUFFIX = ' [%(module)14s:%(lineno)3d] %(message)s'

neo/master/handlers/__init__.py
@@ -15,7 +15,7 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 
-from neo import logging
+import neo
 
 from neo.handler import EventHandler
 from neo.protocol import NodeTypes, NodeStates, Packets
@@ -25,7 +25,7 @@ class MasterHandler(EventHandler):
     """This class implements a generic part of the event handlers."""
 
     def protocolError(self, conn, message):
-        logging.error('Protocol error %s %s' % (message, conn.getAddress()))
+        neo.logging.error('Protocol error %s %s', message, conn.getAddress())
 
     def askPrimary(self, conn):
         if conn.getConnector() is None:
@@ -101,7 +101,7 @@ class BaseServiceHandler(MasterHandler):
         if new_state != NodeStates.BROKEN and was_pending:
             # was in pending state, so drop it from the node manager to forget
             # it and do not set in running state when it comes back
-            logging.info('drop a pending node from the node manager')
+            neo.logging.info('drop a pending node from the node manager')
             self.app.nm.remove(node)
         self.app.broadcastNodesInformation([node])
         # clean node related data in specialized handlers

neo/storage/handlers/__init__.py
@@ -15,7 +15,7 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 
-from neo import logging
+import neo
 
 from neo.handler import EventHandler
 from neo import protocol
@@ -35,7 +35,7 @@ class BaseMasterHandler(EventHandler):
         raise PrimaryFailure('re-election occurs')
 
     def notifyClusterInformation(self, conn, state):
-        logging.warning('ignoring notify cluster information in %s' %
+        neo.logging.warning('ignoring notify cluster information in %s' %
                 self.__class__.__name__)
 
     def notifyLastOID(self, conn, oid):
@@ -48,7 +48,7 @@ class BaseMasterHandler(EventHandler):
         for node_type, addr, uuid, state in node_list:
             if uuid == self.app.uuid:
                 # This is me, do what the master tell me
-                logging.info("I was told I'm %s" %(state))
+                neo.logging.info("I was told I'm %s" %(state))
                 if state in (NodeStates.DOWN, NodeStates.TEMPORARILY_DOWN,
                         NodeStates.BROKEN):
                     conn.close()
@@ -57,7 +57,7 @@ class BaseMasterHandler(EventHandler):
                 elif state == NodeStates.HIDDEN:
                     raise OperationFailure
             elif node_type == NodeTypes.CLIENT and state != NodeStates.RUNNING:
-                logging.info('Notified of non-running client, abort (%r)',
+                neo.logging.info('Notified of non-running client, abort (%r)',
                         dump(uuid))
                 self.app.tm.abortFor(uuid)
 
@@ -86,14 +86,14 @@ class BaseClientAndStorageOperationHandler(EventHandler):
             return
         o = self._askObject(oid, serial, tid)
         if o is None:
-            logging.debug('oid = %s does not exist', dump(oid))
+            neo.logging.debug('oid = %s does not exist', dump(oid))
             p = Errors.OidDoesNotExist(dump(oid))
         elif o is False:
-            logging.debug('oid = %s not found', dump(oid))
+            neo.logging.debug('oid = %s not found', dump(oid))
             p = Errors.OidNotFound(dump(oid))
         else:
             serial, next_serial, compression, checksum, data, data_serial = o
-            logging.debug('oid = %s, serial = %s, next_serial = %s',
+            neo.logging.debug('oid = %s, serial = %s, next_serial = %s',
                           dump(oid), dump(serial), dump(next_serial))
             p = Packets.AnswerObject(oid, serial, next_serial,
                 compression, checksum, data, data_serial)

neo/tests/__init__.py
@@ -20,7 +20,7 @@ import random
 import unittest
 import tempfile
 import MySQLdb
-from neo import logging
+import neo
 from mock import Mock
 from neo import protocol
 from neo.protocol import Packets
@@ -394,7 +394,7 @@ connector_cpt = 0
 
 class DoNothingConnector(Mock):
     def __init__(self, s=None):
-        logging.info("initializing connector")
+        neo.logging.info("initializing connector")
         self.desc = globals()['connector_cpt']
         globals()['connector_cpt'] = globals()['connector_cpt']+ 1
         self.packet_cpt = 0

neo/admin/app.py
@@ -15,7 +15,7 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 
-from neo import logging
+import neo
 
 from neo.node import NodeManager
 from neo.event import EventManager
@@ -68,7 +68,7 @@ class Application(object):
         self.server = config.getBind()
         self.master_addresses = config.getMasters()
 
-        logging.debug('IP address is %s, port is %d', *(self.server))
+        neo.logging.debug('IP address is %s, port is %d', *(self.server))
 
         # The partition table is initialized after getting the number of
         # partitions.
@@ -105,7 +105,7 @@ class Application(object):
                 while True:
                     self.em.poll(1)
             except PrimaryFailure:
-                logging.error('primary master is down')
+                neo.logging.error('primary master is down')
 
 
     def connectToPrimary(self):

neo/admin/handler.py
@@ -15,7 +15,7 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 
-from neo import logging
+import neo
 
 from neo.handler import EventHandler
 from neo import protocol
@@ -42,7 +42,7 @@ class AdminEventHandler(EventHandler):
     """This class deals with events for administrating cluster."""
 
     def askPartitionList(self, conn, min_offset, max_offset, uuid):
-        logging.info("ask partition list from %s to %s for %s" %
+        neo.logging.info("ask partition list from %s to %s for %s" %
                 (min_offset, max_offset, dump(uuid)))
         app = self.app
         # check we have one pt otherwise ask it to PMN
@@ -61,7 +61,7 @@ class AdminEventHandler(EventHandler):
 
 
     def askNodeList(self, conn, node_type):
-        logging.info("ask node list for %s" %(node_type))
+        neo.logging.info("ask node list for %s" %(node_type))
         def node_filter(n):
             return n.getType() is node_type
         node_list = self.app.nm.getList(node_filter)
@@ -70,7 +70,7 @@ class AdminEventHandler(EventHandler):
         conn.answer(p)
 
     def setNodeState(self, conn, uuid, state, modify_partition_table):
-        logging.info("set node state for %s-%s" %(dump(uuid), state))
+        neo.logging.info("set node state for %s-%s" %(dump(uuid), state))
         node = self.app.nm.getByUUID(uuid)
         if node is None:
             raise protocol.ProtocolError('invalid uuid')
@@ -144,7 +144,7 @@ class MasterEventHandler(EventHandler):
     def answerNodeInformation(self, conn):
         # XXX: This will no more exists when the initialization module will be
         # implemented for factorize code (as done for bootstrap)
-        logging.debug("answerNodeInformation")
+        neo.logging.debug("answerNodeInformation")
 
     def notifyPartitionChanges(self, conn, ptid, cell_list):
         self.app.pt.update(ptid, cell_list, self.app.nm)
@@ -176,12 +176,12 @@ class MasterRequestEventHandler(EventHandler):
         client_conn.answer(packet)
 
     def answerClusterState(self, conn, state):
-        logging.info("answerClusterState for a conn")
+        neo.logging.info("answerClusterState for a conn")
         self.app.cluster_state = state
         self._answerNeoCTL(conn, Packets.AnswerClusterState(state))
 
     def answerPartitionTable(self, conn, ptid, row_list):
-        logging.info("answerPartitionTable for a conn")
+        neo.logging.info("answerPartitionTable for a conn")
         client_conn, kw = self.app.dispatcher.pop(conn.getPeerId())
         # sent client the partition table
         self.app.sendPartitionTable(client_conn)

neo/bootstrap.py
@@ -15,7 +15,7 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 
-from neo import logging
+import neo
 from time import sleep
 
 from neo.handler import EventHandler
@@ -106,7 +106,7 @@ class BootstrapManager(EventHandler):
             conn.close()
             return
 
-        logging.info('connected to a primary master node')
+        neo.logging.info('connected to a primary master node')
         conn.ask(Packets.RequestIdentification(self.node_type,
                 self.uuid, self.server, self.name))
 
@@ -120,7 +120,7 @@ class BootstrapManager(EventHandler):
         if self.uuid != your_uuid:
             # got an uuid from the primary master
             self.uuid = your_uuid
-            logging.info('Got a new UUID : %s' % dump(self.uuid))
+            neo.logging.info('Got a new UUID : %s' % dump(self.uuid))
         conn.setUUID(uuid)
 
     def getPrimaryConnection(self, connector_handler):
@@ -128,7 +128,7 @@ class BootstrapManager(EventHandler):
         Primary lookup/connection process.
         Returns when the connection is made.
         """
-        logging.info('connecting to a primary master node')
+        neo.logging.info('connecting to a primary master node')
         em, nm = self.app.em, self.app.nm
         index = 0
         self.current = nm.getMasterList()[0]

neo/client/Storage.py
@@ -163,7 +163,7 @@ class Storage(BaseStorage.BaseStorage,
 
     def pack(self, t, referencesf, gc=False):
         if gc:
-            logging.warning('Garbage Collection is not available in NEO, '
+            neo.logging.warning('Garbage Collection is not available in NEO, '
                 'please use an external tool. Packing without GC.')
         self.app.pack(t)
 

commit 61afbbee596de7f74e5fb14d910237a0b8a3b994

Fix the ability to run individual test files.
This was broken by r2371: it is good not to import it at file top-level,
but it must still be provided.
Also, fix 'No handler could be found for logger "NEO"' error messages,
which where hiding log messages, by using setupLog.
Also, manually set propagate to 0 so NEO messages are not output by root
logger too.

git-svn-id: https://svn.erp5.org/repos/neo/trunk@2376 71dcc9de-d417-0410-9af5-da40c76e7ee4

neo/__init__.py
@@ -17,8 +17,6 @@
 
 import logging as logging_std
 
-# "logging" is available here only once setupLog has been called.
-
 PREFIX = '%(asctime)s %(levelname)-9s %(name)-10s'
 SUFFIX = ' [%(module)14s:%(lineno)3d] %(message)s'
 
@@ -39,4 +37,8 @@ def setupLog(name='NEO', filename=None, verbose=False):
     handler.setFormatter(logging_std.Formatter(fmt))
     logging.setLevel(level)
     logging.addHandler(handler)
+    logging.propagate = 0
+
+# Create default logger
+setupLog()
 