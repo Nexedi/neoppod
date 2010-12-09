0c7e1dd43031a2cf98c880594dd7842db5c93fea
Grégory Wisniewski <gregory@nexedi.com>
2010-12-09 17:17:51 +0000

    Replicate the current chunk if the peer answered less than min range.
    git-svn-id: https://svn.erp5.org/repos/neo/trunk@2501 71dcc9de-d417-0410-9af5-da40c76e7ee4

--- neo/storage/handlers/replication.py
+++ neo/storage/handlers/replication.py
@@ -227,7 +227,7 @@ class ReplicationHandler(EventHandler):
                 params = (next_boundary, )
         else:
             # We must recheck current chunk.
-            if length <= MIN_RANGE_LENGTH:
+            if count <= MIN_RANGE_LENGTH:
                 # We are already at minimum chunk length, replicate.
                 action = CHECK_REPLICATE
                 params = (recheck_min_boundary, )
