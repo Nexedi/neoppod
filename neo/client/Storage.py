aebcf4a48e6af5d46cae9d0f18c1ac9f562409a9
Julien Muchembled <jm@nexedi.com>
2011-06-22 15:55:01 +0000

    client: make sure neo.lib.python is imported before functools on Python 2.4
    git-svn-id: https://svn.erp5.org/repos/neo/trunk@2797 71dcc9de-d417-0410-9af5-da40c76e7ee4

--- neo/client/Storage.py
+++ neo/client/Storage.py
@@ -15,12 +15,12 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 
-from functools import wraps
 from ZODB import BaseStorage, ConflictResolution, POSException
 from zope.interface import implements
 import ZODB.interfaces
 
-import neo
+import neo.lib
+from functools import wraps
 from neo.lib import setupLog
 from neo.lib.util import add64
 from neo.lib.protocol import ZERO_TID
