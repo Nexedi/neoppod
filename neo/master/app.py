Nexedi/neoppod

commit b5309c783557ff0b7f4600ce24b2b38476344345

Simplify code.

neo/master/app.py
@@ -403,10 +403,7 @@ class Application(object):
 
     def isValidUUID(self, uuid, addr):
         node = self.nm.getByUUID(uuid)
-        if node is not None and node.getAddress() is not None \
-                and node.getAddress() != addr:
-            return False
-        return uuid != self.uuid and uuid is not None
+        return node is None or node.getAddress() in (None, addr)
 
     def getClusterState(self):
         return self.cluster_state

commit 30462e364e4f32cb51ef6baffc98b9b712386850

Always reject my own UUID.
Fixes a regression introduced in b5309c7 .

neo/master/app.py
@@ -402,6 +402,8 @@ class Application(object):
         return prefix + uuid
 
     def isValidUUID(self, uuid, addr):
+        if uuid == self.uuid:
+            return False
         node = self.nm.getByUUID(uuid)
         return node is None or node.getAddress() in (None, addr)
 