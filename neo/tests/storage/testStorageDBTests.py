4456b532b18e3dcd31a9ca46eb1b238327ab6f07
Vincent Pelletier <vincent@nexedi.com>
2012-02-10 17:41:54 +0100

    Unused import.

--- neo/tests/storage/testStorageDBTests.py
+++ neo/tests/storage/testStorageDBTests.py
@@ -21,7 +21,6 @@ from neo.lib.util import dump, p64, u64
 from neo.lib.protocol import CellStates, ZERO_OID, ZERO_TID
 from .. import NeoUnitTestBase
 from neo.lib.exception import DatabaseFailure
-from neo.storage.database.mysqldb import MySQLDatabaseManager
 
 MAX_TID = '\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFE' # != INVALID_TID
 
