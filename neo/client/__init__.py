8afbdd53a5f60ebd197be4fe329fc04db522872e
Julien Muchembled <jm@nexedi.com>
2013-11-13 14:08:17 +0100

    client: detect if monkey-patched code changes

--- neo/client/__init__.py
+++ neo/client/__init__.py
@@ -12,17 +12,24 @@
 #
 ##############################################################################
 
-# At the moment, no ZODB release include the following patches.
-# Later, this must be replaced by some detection mechanism.
 if 1:
-
+    from hashlib import md5
     from ZODB.Connection import Connection
+
+    def _check(f, *args):
+        h = md5(f.func_code.co_code).hexdigest()
+        assert h in args, h
 
     # Allow serial to be returned as late as tpc_finish
     #
     # This makes possible for storage to allocate serial inside tpc_finish,
     # removing the requirement to serialise second commit phase (tpc_vote
     # to tpc_finish/tpc_abort).
+
+    _check(Connection.tpc_finish,
+        'f50ed2e5a74f584fa1ecaf1cd70a6b15', # Python 2.6
+        'ab9b1b8d82c40e5fffa84f7bc4ea3a8b', # Python 2.7
+        )
 
     def tpc_finish(self, transaction):
         """Indicate confirmation that the transaction is done."""
@@ -39,6 +46,7 @@ if 1:
 #       to be able to read any updated data until we've had a chance
 #       to send an invalidation message to all of the other
 #       connections!
+        # <patch>
         serial = self._storage.tpc_finish(transaction, callback)
         if serial is not None:
             assert isinstance(serial, str), repr(serial)
@@ -49,6 +57,7 @@ if 1:
                     if obj is not None and obj._p_changed is not None:
                         obj._p_changed = 0
                         obj._p_serial = serial
+        # </patch>
         self._tpc_cleanup()
 
     Connection.tpc_finish = tpc_finish
@@ -61,6 +70,11 @@ if 1:
     # For example, we know we are up-to-date after a successful commit,
     # so this should not be done in afterCompletion(), and anyway, we don't
     # know any legitimate use of DB access outside a transaction.
+
+    _check(Connection.afterCompletion,
+        '70dfc3df8a455d9f663dec619c607eb5', # Python 2.6
+        'cd3a080b80fd957190ff3bb867149448', # Python 2.7
+        )
 
     def afterCompletion(self, *ignored):
         self._readCurrent.clear()
