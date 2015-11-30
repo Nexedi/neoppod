Nexedi/neoppod

commit 83fe64bf104908e503d40634b97c36c5c4b4b835

storage: fix pruning of data when deleting partial transactions during verification

neo/storage/database/mysqldb.py
@@ -562,14 +562,13 @@ class MySQLDatabaseManager(DatabaseManager):
         getPartition = self._getPartition
         q = self.query
         sql = " FROM tobj WHERE tid=%d" % tid
-        data_id_list = [x for x, in q("SELECT data_id" + sql) if x]
-        self.releaseData(data_id_list)
+        data_id_set = {x for x, in q("SELECT data_id" + sql) if x}
+        self.releaseData(data_id_set)
         q("DELETE" + sql)
         q("""DELETE FROM ttrans WHERE tid = %d""" % tid)
         q("""DELETE FROM trans WHERE `partition` = %d AND tid = %d""" %
             (getPartition(tid), tid))
         # delete from obj using indexes
-        data_id_set = set()
         for oid in oid_list:
             oid = u64(oid)
             sql = " FROM obj WHERE `partition`=%d AND oid=%d AND tid=%d" \

neo/storage/database/sqlite.py
@@ -424,14 +424,13 @@ class SQLiteDatabaseManager(DatabaseManager):
         getPartition = self._getPartition
         q = self.query
         sql = " FROM tobj WHERE tid=?"
-        data_id_list = [x for x, in q("SELECT data_id" + sql, (tid,)) if x]
-        self.releaseData(data_id_list)
+        data_id_set = {x for x, in q("SELECT data_id" + sql, (tid,)) if x}
+        self.releaseData(data_id_set)
         q("DELETE" + sql, (tid,))
         q("DELETE FROM ttrans WHERE tid=?", (tid,))
         q("DELETE FROM trans WHERE partition=? AND tid=?",
             (getPartition(tid), tid))
         # delete from obj using indexes
-        data_id_set = set()
         for oid in oid_list:
             oid = u64(oid)
             sql = " FROM obj WHERE partition=? AND oid=? AND tid=?"

neo/tests/threaded/test.py
@@ -438,6 +438,14 @@ class Test(NEOThreadedTest):
         finally:
             cluster.stop()
         cluster.reset()
+        di0 = s0.getDataLockInfo()
+        k, = (k for k, v in di0.iteritems() if v == 1)
+        di0[k] = 0 # r[2] = 'ok'
+        self.assertEqual(di0.values(), [0, 0, 0, 0, 0])
+        di1 = s1.getDataLockInfo()
+        k, = (k for k, v in di1.iteritems() if v == 1)
+        del di1[k] # x.value = 1
+        self.assertEqual(di1.values(), [0])
         try:
             cluster.start()
             t, c = cluster.getTransaction()
@@ -445,6 +453,8 @@ class Test(NEOThreadedTest):
             self.assertEqual(r[0].value, 0)
             self.assertEqual(r[1].value, 0)
             self.assertEqual(r[2], 'ok')
+            self.assertEqual(di0, s0.getDataLockInfo())
+            self.assertEqual(di1, s1.getDataLockInfo())
         finally:
             cluster.stop()
 

commit cff279af125242e41c527b152c6ca1b621b76297

fixup! storage: fix pruning of data when deleting partial transactions during verification
This fixes a regression in commit 83fe64bf104908e503d40634b97c36c5c4b4b835
when ttrans has several rows to the same data_id.

neo/storage/database/mysqldb.py
@@ -562,21 +562,22 @@ class MySQLDatabaseManager(DatabaseManager):
         getPartition = self._getPartition
         q = self.query
         sql = " FROM tobj WHERE tid=%d" % tid
-        data_id_set = {x for x, in q("SELECT data_id" + sql) if x}
-        self.releaseData(data_id_set)
+        data_id_list = [x for x, in q("SELECT data_id" + sql) if x]
+        self.releaseData(data_id_list)
         q("DELETE" + sql)
         q("""DELETE FROM ttrans WHERE tid = %d""" % tid)
         q("""DELETE FROM trans WHERE `partition` = %d AND tid = %d""" %
             (getPartition(tid), tid))
         # delete from obj using indexes
+        data_id_list = set(data_id_list)
         for oid in oid_list:
             oid = u64(oid)
             sql = " FROM obj WHERE `partition`=%d AND oid=%d AND tid=%d" \
                % (getPartition(oid), oid, tid)
-            data_id_set.update(*q("SELECT data_id" + sql))
+            data_id_list.update(*q("SELECT data_id" + sql))
             q("DELETE" + sql)
-        data_id_set.discard(None)
-        self._pruneData(data_id_set)
+        data_id_list.discard(None)
+        self._pruneData(data_id_list)
 
     def deleteObject(self, oid, serial=None):
         u64 = util.u64

neo/storage/database/sqlite.py
@@ -425,21 +425,22 @@ class SQLiteDatabaseManager(DatabaseManager):
         getPartition = self._getPartition
         q = self.query
         sql = " FROM tobj WHERE tid=?"
-        data_id_set = {x for x, in q("SELECT data_id" + sql, (tid,)) if x}
-        self.releaseData(data_id_set)
+        data_id_list = [x for x, in q("SELECT data_id" + sql, (tid,)) if x]
+        self.releaseData(data_id_list)
         q("DELETE" + sql, (tid,))
         q("DELETE FROM ttrans WHERE tid=?", (tid,))
         q("DELETE FROM trans WHERE partition=? AND tid=?",
             (getPartition(tid), tid))
         # delete from obj using indexes
+        data_id_list = set(data_id_list)
         for oid in oid_list:
             oid = u64(oid)
             sql = " FROM obj WHERE partition=? AND oid=? AND tid=?"
             args = getPartition(oid), oid, tid
-            data_id_set.update(*q("SELECT data_id" + sql, args))
+            data_id_list.update(*q("SELECT data_id" + sql, args))
             q("DELETE" + sql, args)
-        data_id_set.discard(None)
-        self._pruneData(data_id_set)
+        data_id_list.discard(None)
+        self._pruneData(data_id_list)
 
     def deleteObject(self, oid, serial=None):
         oid = util.u64(oid)