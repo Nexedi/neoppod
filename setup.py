f180b00ecb672fe2e73121d9bb51dbe43b310e79
Julien Muchembled <jm@nexedi.com>
2015-12-02 15:26:41 +0100

    Release version 1.6

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ else:
 
 setup(
     name = 'neoppod',
-    version = '1.5.1',
+    version = '1.6',
     description = __doc__.strip(),
     author = 'NEOPPOD',
     author_email = 'neo-dev@erp5.org',
