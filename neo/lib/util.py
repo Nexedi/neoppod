7e2f3cd8f9c8baa97e05e058b7c7bac915f0a254
Ilmanfordinner <ilmanfordinner@gmail.com>
2016-10-21 19:10:38 +0300

    Small miscalculation

--- neo/lib/util.py
+++ neo/lib/util.py
@@ -155,7 +155,7 @@ def timeStringFromTID(ptid):
         seconds-=60
     fractionSeconds = seconds - int(seconds)
 
-    return '%04d-%02d-%02d %02d:%02d:%02d%09.6f' \
+    return '%04d-%02d-%02d %02d:%02d:%02d%07.6f' \
           %(timeTuple[0][0], timeTuple[0][1], timeTuple[0][2], timeTuple[0][3],\
             timeTuple[0][4], int(seconds), fractionSeconds)
 
