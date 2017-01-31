c2d88ba0c5519beb03f2a7af1aa9f5be38761d37
Kirill Smelkov <kirr@nexedi.com>
2017-01-31 12:36:39 +0300

    .

---
+++ t/NOTES.go
@@ -0,0 +1,12 @@
+- custom memory manager is required to avoid performance hit on e.g. []byte alloc/dealloc
+
+  -> go-slab
+
+- interfaces conversion in hot codepaths are costly:
+
+  e.g. having r as io.ReadSeeker and converting it to io.Reader e.g. this way:
+
+
+	io.ReadFull(r, ...)
+
+  calls convI2I all the time (no caching) which is not negliable
