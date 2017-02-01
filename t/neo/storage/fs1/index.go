e797c2fec24900446af7852d892fbb0dab0ece2b
Kirill Smelkov <kirr@nexedi.com>
2017-02-01 21:52:20 +0300

    .

---
+++ t/neo/storage/fs1/index.go
@@ -0,0 +1,23 @@
+// Copyright (C) 2017  Nexedi SA and Contributors.
+//                     Kirill Smelkov <kirr@nexedi.com>
+//
+// This program is free software: you can Use, Study, Modify and Redistribute
+// it under the terms of the GNU General Public License version 2, or (at your
+// option) any later version, as published by the Free Software Foundation.
+//
+// This program is distributed WITHOUT ANY WARRANTY; without even the implied
+// warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+//
+// See COPYING file for full licensing terms.
+//
+// XXX based on code from ZODB ?
+
+// FileStorage. Index
+
+import (
+)
+
+// fsIndex is Oid -> Tid's position mapping used to associate Oid with latest
+// transaction which changed it.	TODO more text
+type fsIndex struct {
+}
