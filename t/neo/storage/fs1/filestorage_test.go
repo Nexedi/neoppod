3b98998d9fb9ca5d79819db7455613beaacb850f
Kirill Smelkov <kirr@nexedi.com>
2017-02-22 21:40:45 +0300

    .

--- t/neo/storage/fs1/filestorage_test.go
+++ t/neo/storage/fs1/filestorage_test.go
@@ -12,13 +12,22 @@ import (
 
 // one entry inside transaction
 type txnEntry struct {
-	header	DataHeader
-	data	[]byte
+	Header	DataHeader
+	rawData	[]byte		// what is on disk, e.g. it can be backpointer
+	data	[]byte		// data client should see on load; nil means same as RawData
+}
+
+func (txe *txnEntry) Data() []byte {
+	data := txe.data
+	if data == nil {
+		data = txe.rawData
+	}
+	return data
 }
 
 type dbEntry struct {
-	header	TxnHeader
-	entryv	[]txnEntry
+	Header	TxnHeader
+	Entryv	[]txnEntry
 }
 
 func TestLoad(t *testing.T) {
@@ -28,11 +37,8 @@ func TestLoad(t *testing.T) {
 	}
 
 	for _, dbe := range _1fs_dbEntryv {
-		for _, txe := range dbe.entryv {
-			txh := txe.header
-			if txh.DataLen == 0 {
-				continue	// FIXME skipping backpointers
-			}
+		for _, txe := range dbe.Entryv {
+			txh := txe.Header
 
 			xid := zodb.Xid{zodb.XTid{txh.Tid, false}, txh.Oid}	// loadSerial
 			data, tid, err := fs.Load(xid)
@@ -42,8 +48,8 @@ func TestLoad(t *testing.T) {
 			if tid != txh.Tid {
 				t.Errorf("load %v: returned tid unexpected: %v", xid)
 			}
-			if !bytes.Equal(data, txe.data) {
-				t.Errorf("load %v: different data:\nhave: %s\nwant: %s", xid, strconv.Quote(string(data)), strconv.Quote(string(txe.data)))
+			if !bytes.Equal(data, txe.Data()) {
+				t.Errorf("load %v: different data:\nhave: %s\nwant: %s", xid, strconv.Quote(string(data)), strconv.Quote(string(txe.Data())))
 			}
 		}
 	}
