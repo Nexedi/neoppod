Nexedi/neoppod

commit a4c06242e3ddb53e7c11895d895823e1c30db4e6

Review aborting of transactions

neo/client/app.py
@@ -595,19 +595,16 @@ class Application(ThreadedApplication):
         txn_context = self._txn_container.pop(transaction)
         if txn_context is None:
             return
-        p = Packets.AbortTransaction(txn_context['ttid'])
-        # cancel transaction on all those nodes
-        conns = [self.master_conn]
-        for uuid in txn_context['involved_nodes']:
-            node = self.nm.getByUUID(uuid)
-            if node is not None:
-                conns.append(self.cp.getConnForNode(node))
-        for conn in conns:
-            if conn is not None:
-                try:
-                    conn.notify(p)
-                except ConnectionClosed:
-                    pass
+        try:
+            notify = self.master_conn.notify
+        except AttributeError:
+            pass
+        else:
+            try:
+                notify(Packets.AbortTransaction(txn_context['ttid'],
+                                                txn_context['involved_nodes']))
+            except ConnectionClosed:
+                pass
         # We don't need to flush queue, as it won't be reused by future
         # transactions (deleted on next line & indexed by transaction object
         # instance).

neo/lib/protocol.py
@@ -976,10 +976,11 @@ class StoreObject(Packet):
 
 class AbortTransaction(Packet):
     """
-    Abort a transaction. C -> S, PM.
+    Abort a transaction. C -> PM -> S.
     """
     _fmt = PStruct('abort_transaction',
         PTID('tid'),
+        PFUUIDList, # unused for PM -> S
     )
 
 class StoreTransaction(Packet):

neo/storage/handlers/client.py
@@ -57,9 +57,6 @@ class ClientOperationHandler(EventHandler):
                 compression, checksum, data, data_serial)
         conn.answer(p)
 
-    def abortTransaction(self, conn, ttid):
-        self.app.tm.abort(ttid)
-
     def askStoreTransaction(self, conn, ttid, *txn_info):
         self.app.tm.register(conn, ttid)
         self.app.tm.vote(ttid, txn_info)
@@ -201,7 +198,6 @@ class ClientReadOnlyOperationHandler(ClientOperationHandler):
         conn.answer(Errors.ReadOnlyAccess(
             'read-only access because cluster is in backuping mode'))
 
-    abortTransaction        = _readOnly
     askStoreTransaction     = _readOnly
     askVoteTransaction      = _readOnly
     askStoreObject          = _readOnly

neo/storage/handlers/master.py
@@ -57,6 +57,10 @@ class MasterOperationHandler(BaseMasterHandler):
     def notifyUnlockInformation(self, conn, ttid):
         self.app.tm.unlock(ttid)
 
+    def abortTransaction(self, conn, ttid, _):
+        self.app.tm.abort(ttid)
+        self.app.replicator.transactionFinished(ttid)
+
     def askPack(self, conn, tid):
         app = self.app
         logging.info('Pack started, up to %s...', dump(tid))

neo/tests/threaded/test.py
@@ -32,7 +32,7 @@ from neo.lib.connection import ServerConnection, MTClientConnection
 from neo.lib.exception import DatabaseFailure, StoppedOperation
 from neo.lib.protocol import CellStates, ClusterStates, NodeStates, Packets, \
     ZERO_OID, ZERO_TID
-from .. import expectedFailure, Patch
+from .. import expectedFailure, Patch, TransactionalResource
 from . import ConnectionFilter, LockLock, NEOThreadedTest, with_cluster
 from neo.lib.util import add64, makeChecksum, p64, u64
 from neo.client.exception import NEOPrimaryMasterLost, NEOStorageError
@@ -1462,6 +1462,98 @@ class Test(NEOThreadedTest):
             r[x]._p_deactivate()
             value_list.append(r[x].value)
         self.assertEqual(value_list, range(3))
+
+    @with_cluster(replicas=1, partitions=3, storage_count=3)
+    def testMasterArbitratingVote(self, cluster):
+        """
+        p\S 1  2  3
+        0   U  U  .
+        1   .  U  U
+        2   U  .  U
+
+        With the above setup, check when a client C1 fails to connect to S2
+        and another C2 fails to connect to S1.
+
+        For the first 2 scenarios:
+        - C1 first votes (the master accepts)
+        - C2 vote is delayed until C1 decides to finish or abort
+        """
+        def delayAbort(conn, packet):
+            return isinstance(packet, Packets.AbortTransaction)
+        def noConnection(jar, storage):
+            return Patch(jar.db().storage.app.cp,
+                getConnForNode=lambda orig, node:
+                    None if node.getUUID() == storage.uuid else orig(node))
+        def c1_vote(txn):
+            def vote(orig, *args):
+                result = orig(*args)
+                ll()
+                return result
+            with LockLock() as ll, Patch(cluster.master.tm, vote=vote):
+                commit2.start()
+                ll()
+            if c1_aborts:
+                raise Exception
+        pt = [{x.getUUID() for x in x}
+            for x in cluster.master.pt.partition_list]
+        cluster.storage_list.sort(key=lambda x:
+            (x.uuid not in pt[0], x.uuid in pt[1]))
+        pt = 'UU.|.UU|U.U'
+        self.assertPartitionTable(cluster, pt)
+        s1, s2, s3 = cluster.storage_list
+        t1, c1 = cluster.getTransaction()
+        with cluster.newClient(1) as db:
+            t2, c2 = cluster.getTransaction(db)
+            with noConnection(c1, s2), noConnection(c2, s1):
+                cluster.client.cp.connection_dict[s2.uuid].close()
+                self.tic()
+                for c1_aborts in 0, 1:
+                    # 0: C1 finishes, C2 vote fails
+                    # 1: C1 aborts, C2 finishes
+                    #
+                    # Although we try to modify the same oid, there's no
+                    # conflict because each storage node sees a single
+                    # and different transaction: vote to storages is done
+                    # in parallel, and the master must be involved as an
+                    # arbitrator, which ultimately rejects 1 of the 2
+                    # transactions, preferably before the second phase of
+                    # the commit.
+                    t1.begin(); c1.root()._p_changed = 1
+                    t2.begin(); c2.root()._p_changed = 1
+                    commit2 = self.newPausedThread(t2.commit)
+                    TransactionalResource(t1, 1, tpc_vote=c1_vote)
+                    with ConnectionFilter() as f:
+                        if not c1_aborts:
+                            f.add(delayAbort)
+                        f.delayAskFetchTransactions(lambda _:
+                            f.discard(delayAbort))
+                        try:
+                            t1.commit()
+                            self.assertFalse(c1_aborts)
+                        except Exception:
+                            self.assertTrue(c1_aborts)
+                        try:
+                            commit2.join()
+                            self.assertTrue(c1_aborts)
+                        except NEOStorageError:
+                            self.assertFalse(c1_aborts)
+                        self.tic()
+                        self.assertPartitionTable(cluster,
+                            'OU.|.UU|O.U' if c1_aborts else 'UO.|.OU|U.U')
+                    self.tic()
+                    self.assertPartitionTable(cluster, pt)
+                # S3 fails while C1 starts to finish
+                with ConnectionFilter() as f:
+                    f.add(lambda conn, packet: conn.getUUID() == s3.uuid and
+                        isinstance(packet, Packets.AcceptIdentification))
+                    t1.begin(); c1.root()._p_changed = 1
+                    TransactionalResource(t1, 0, tpc_finish=lambda *_:
+                        cluster.master.nm.getByUUID(s3.uuid)
+                        .getConnection().close())
+                    self.assertRaises(NEOStorageError, t1.commit)
+                    self.assertPartitionTable(cluster, 'UU.|.UO|U.O')
+                self.tic()
+                self.assertPartitionTable(cluster, pt)
 
 if __name__ == "__main__":
     unittest.main()

neo/master/app.py
@@ -585,6 +585,6 @@ class Application(BaseApplication):
     def isStorageReady(self, uuid):
         return uuid in self.storage_ready_dict
 
-    def getStorageReadySet(self, readiness):
+    def getStorageReadySet(self, readiness=float('inf')):
         return {k for k, v in self.storage_ready_dict.iteritems()
                   if v <= readiness}

neo/master/handlers/client.py
@@ -74,6 +74,12 @@ class ClientServiceHandler(MasterHandler):
                 node.ask(p, timeout=60)
         else:
             conn.answer(Errors.IncompleteTransaction())
+            # It's simpler to abort automatically rather than asking the client
+            # to send a notification on tpc_abort, since it would have keep the
+            # transaction longer in list of transactions.
+            # This should happen so rarely that we don't try to minimize the
+            # number of abort notifications by looking the modified partitions.
+            self.abortTransaction(conn, ttid, app.getStorageReadySet())
 
     def askFinalTID(self, conn, ttid):
         tm = self.app.tm
@@ -102,9 +108,24 @@ class ClientServiceHandler(MasterHandler):
         else:
             conn.answer(Packets.AnswerPack(False))
 
-    def abortTransaction(self, conn, tid):
-        # BUG: The replicator may wait this transaction to be finished.
-        self.app.tm.abort(tid, conn.getUUID())
+    def abortTransaction(self, conn, tid, uuid_list):
+        # Consider a failure when the connection between the storage and the
+        # client breaks while the answer to the first write is sent back.
+        # In other words, the client can not know the exact set of nodes that
+        # know this transaction, and it sends us all nodes it considered for
+        # writing.
+        # We must also add those that are waiting for this transaction to be
+        # finished (returned by tm.abort), because they may have join the
+        # cluster after that the client started to abort.
+        app = self.app
+        involved = app.tm.abort(tid, conn.getUUID())
+        involved.update(uuid_list)
+        involved.intersection_update(app.getStorageReadySet())
+        if involved:
+            p = Packets.AbortTransaction(tid, ())
+            getByUUID = app.nm.getByUUID
+            for involved in involved:
+                getByUUID(involved).notify(p)
 
 
 # like ClientServiceHandler but read-only & only for tid <= backup_tid

neo/tests/__init__.py
@@ -37,6 +37,7 @@ from time import time
 from struct import pack, unpack
 from unittest.case import _ExpectedFailure, _UnexpectedSuccess
 try:
+    from transaction.interfaces import IDataManager
     from ZODB.utils import newTid
 except ImportError:
     pass
@@ -376,6 +377,30 @@ class NeoUnitTestBase(NeoTestBase):
         self.assertTrue(isinstance(packet, protocol.Packet))
         self.assertEqual(type(packet), packet_type)
         return packet
+
+
+class TransactionalResource(object):
+
+    class _sortKey(object):
+
+        def __init__(self, last):
+            self._last = last
+
+        def __cmp__(self, other):
+            assert type(self) is not type(other), other
+            return 1 if self._last else -1
+
+    def __init__(self, txn, last, **kw):
+        self.sortKey = lambda: self._sortKey(last)
+        for k in kw:
+            assert callable(IDataManager.get(k)), k
+        self.__dict__.update(kw)
+        txn.get().join(self)
+
+    def __getattr__(self, attr):
+        if callable(IDataManager.get(attr)):
+            return lambda *_: None
+        return self.__getattribute__(attr)
 
 
 class Patch(object):

commit 9b33b1db82e3ab4b8e668b17f9491dbe30a0a9c7

Fix oids remaining write-locked forever
This happened in 2 cases:
- Commit a4c06242e3ddb53e7c11895d895823e1c30db4e6 ("Review aborting of
  transactions") introduced a race condition causing oids to remain
  write-locked forever after that the transaction modifying them is aborted.
- An unfinished transaction is not locked/unlocked during tpc_finish: oids
  must be unlocked when being notified that the transaction is finished.

neo/client/app.py
@@ -549,6 +549,24 @@ class Application(ThreadedApplication):
         txn_context = self._txn_container.pop(transaction)
         if txn_context is None:
             return
+        # We want that the involved nodes abort a transaction after any
+        # other packet sent by the client for this transaction. IOW, if we
+        # already have a connection with a storage node, potentially with
+        # a pending write, aborting only via the master may lead to a race
+        # condition. The consequence would be that storage nodes lock oids
+        # forever.
+        p = Packets.AbortTransaction(txn_context.ttid, ())
+        for uuid in txn_context.involved_nodes:
+            try:
+                self.cp.connection_dict[uuid].notify(p)
+            except (KeyError, ConnectionClosed):
+                pass
+        # Because we want to be sure that the involved nodes are notified,
+        # we still have to send the full list to the master. Most of the
+        # time, the storage nodes get 2 AbortTransaction packets, and the
+        # second one is rarely useful. Another option would be that the
+        # storage nodes keep a list of aborted transactions, but the
+        # difficult part would be to avoid a memory leak.
         try:
             notify = self.master_conn.notify
         except AttributeError:

neo/lib/protocol.py
@@ -20,7 +20,7 @@ import traceback
 from cStringIO import StringIO
 from struct import Struct
 
-PROTOCOL_VERSION = 10
+PROTOCOL_VERSION = 11
 
 # Size restrictions.
 MIN_PACKET_SIZE = 10
@@ -1033,11 +1033,11 @@ class StoreObject(Packet):
 
 class AbortTransaction(Packet):
     """
-    Abort a transaction. C -> PM -> S.
+    Abort a transaction. C -> S and C -> PM -> S.
     """
     _fmt = PStruct('abort_transaction',
         PTID('tid'),
-        PFUUIDList, # unused for PM -> S
+        PFUUIDList, # unused for * -> S
     )
 
 class StoreTransaction(Packet):

neo/storage/handlers/__init__.py
@@ -20,7 +20,17 @@ from neo.lib.handler import EventHandler
 from neo.lib.exception import PrimaryFailure, StoppedOperation
 from neo.lib.protocol import uuid_str, NodeStates, NodeTypes, Packets
 
-class BaseMasterHandler(EventHandler):
+class BaseHandler(EventHandler):
+
+    def notifyTransactionFinished(self, conn, ttid, max_tid):
+        app = self.app
+        app.tm.abort(ttid)
+        app.replicator.transactionFinished(ttid, max_tid)
+
+    def abortTransaction(self, conn, ttid, _):
+        self.notifyTransactionFinished(conn, ttid, None)
+
+class BaseMasterHandler(BaseHandler):
 
     def connectionLost(self, conn, new_state):
         if self.app.listening_conn: # if running

neo/storage/handlers/client.py
@@ -15,18 +15,19 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from neo.lib import logging
-from neo.lib.handler import DelayEvent, EventHandler
+from neo.lib.handler import DelayEvent
 from neo.lib.util import dump, makeChecksum, add64
 from neo.lib.protocol import Packets, Errors, ProtocolError, \
     ZERO_HASH, INVALID_PARTITION
 from ..transactions import ConflictError, NotRegisteredError
+from . import BaseHandler
 import time
 
 # Log stores taking (incl. lock delays) more than this many seconds.
 # Set to None to disable.
 SLOW_STORE = 2
 
-class ClientOperationHandler(EventHandler):
+class ClientOperationHandler(BaseHandler):
 
     def askTransactionInformation(self, conn, tid):
         t = self.app.dm.getTransaction(tid)

neo/storage/handlers/master.py
@@ -31,9 +31,6 @@ class MasterOperationHandler(BaseMasterHandler):
             dm._setBackupTID(dm.getLastIDs()[0] or ZERO_TID)
             dm.commit()
 
-    def notifyTransactionFinished(self, conn, *args):
-        self.app.replicator.transactionFinished(*args)
-
     def notifyPartitionChanges(self, conn, ptid, cell_list):
         """This is very similar to Send Partition Table, except that
        the information is only about changes from the previous."""
@@ -56,10 +53,6 @@ class MasterOperationHandler(BaseMasterHandler):
 
     def notifyUnlockInformation(self, conn, ttid):
         self.app.tm.unlock(ttid)
-
-    def abortTransaction(self, conn, ttid, _):
-        self.app.tm.abort(ttid)
-        self.app.replicator.transactionFinished(ttid)
 
     def askPack(self, conn, tid):
         app = self.app

neo/tests/threaded/test.py
@@ -1567,6 +1567,21 @@ class Test(NEOThreadedTest):
                 self.tic()
                 self.assertPartitionTable(cluster, pt)
 
+    @with_cluster()
+    def testAbortTransaction(self, cluster):
+        t, c = cluster.getTransaction()
+        r = c.root()
+        r._p_changed = 1
+        def abort(_):
+            raise Exception
+        TransactionalResource(t, 0, tpc_vote=abort)
+        with cluster.client.filterConnection(cluster.storage) as cs:
+            cs.delayAskStoreObject()
+            self.assertRaises(Exception, t.commit)
+        t.begin()
+        r._p_changed = 1
+        t.commit()
+
     @with_cluster(replicas=1)
     def testPartialConflict(self, cluster):
         """