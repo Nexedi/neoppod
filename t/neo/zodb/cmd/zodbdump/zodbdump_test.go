44cb53de595a26d2a1b7ad3ac67bcbe10d5948a1
Kirill Smelkov <kirr@nexedi.com>
2017-03-07 00:38:38 +0300

    .

--- t/neo/zodb/cmd/zodbdump/zodbdump_test.go
+++ t/neo/zodb/cmd/zodbdump/zodbdump_test.go
@@ -1,3 +1,40 @@
+// TODO copyright/license
+
 package main
 
-//go:generate sh -c "python2 -m zodbtool.zodbdump testdata/1.conf >testdata/1.zdump"
+//go:generate sh -c "python2 -m zodbtool.zodbdump testdata/1.conf >testdata/1.zdump.ok"
+
+import (
+	"bytes"
+	"io/ioutil"
+	"testing"
+
+	"../../../storage/fs1"
+	"../../../zodb"
+
+	"lab.nexedi.com/kirr/go123/exc"
+)
+
+func TestZodbDump(t *testing.T) {
+	buf := bytes.Buffer{}
+	fs, err := fs1.Open("../../../storage/fs1/testdata/1.fs")	// XXX read-only, path?
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	defer exc.XRun(fs.Close)
+
+	err = zodbDump(&buf, fs, 0, zodb.TidMax, false)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	dumpOk, err := ioutil.ReadFile("testdata/1.zdump.ok")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if !bytes.Equal(dumpOk, buf.Bytes()) {
+		t.Errorf("dump different TODO show diff")	// XXX github.com/sergi/go-diff.git
+	}
+}
