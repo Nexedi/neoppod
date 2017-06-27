789c2941f08434da9d780fa857d606dae71384e1
Kirill Smelkov <kirr@nexedi.com>
2017-06-27 13:45:36 +0300

    .

---
+++ go/xcommon/x.go
@@ -0,0 +1,64 @@
+
+// traceevent: traceConnRecv(c *Conn, msg Msg)	XXX better raw .Text (e.g. comments)
+
+
+type _t_traceConnRecv struct {
+	tracing.Probe
+	probefunc     func(c *Conn, msg Msg)
+}
+
+
+var _traceConnRecv *_t_traceConnRecv
+
+
+func traceConnRecv(c *Conn, msg Msg) {
+	if _traceConnRecv != nil {
+		_traceConnRecv_run(c, msg)
+	}
+}
+
+
+func _traceConnRecvc, msg_run(c, msg) {
+	for p := _traceConnRecv; p != nil; p = (*_t_traceConnRecv)(unsafe.Pointer(p.Next())) {
+		p.probefunc(c, msg)
+	}
+}
+
+
+func traceConnRecv_Attach(pg *tracing.ProbeGroup, probe func({31 [0xc42358be80 0xc42358bec0] 56})) *tracing.Probe {
+	p := _t_traceConnRecv{probefunc: probe}
+	tracing.AttachProbe(pg, (**tracing.Probe)(unsafe.Pointer(&_traceConnRecv), &p.Probe)
+	return &p.Probe
+}
+
+// traceevent: traceConnSend(c *Conn, msg Msg)	XXX better raw .Text (e.g. comments)
+
+
+type _t_traceConnSend struct {
+	tracing.Probe
+	probefunc     func(c *Conn, msg Msg)
+}
+
+
+var _traceConnSend *_t_traceConnSend
+
+
+func traceConnSend(c *Conn, msg Msg) {
+	if _traceConnSend != nil {
+		_traceConnSend_run(c, msg)
+	}
+}
+
+
+func _traceConnSendc, msg_run(c, msg) {
+	for p := _traceConnSend; p != nil; p = (*_t_traceConnSend)(unsafe.Pointer(p.Next())) {
+		p.probefunc(c, msg)
+	}
+}
+
+
+func traceConnSend_Attach(pg *tracing.ProbeGroup, probe func({31 [0xc42358bf40 0xc42358bf80] 48})) *tracing.Probe {
+	p := _t_traceConnSend{probefunc: probe}
+	tracing.AttachProbe(pg, (**tracing.Probe)(unsafe.Pointer(&_traceConnSend), &p.Probe)
+	return &p.Probe
+}
