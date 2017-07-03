49274b42554b2b59b711ae9c82b9b3f0e138f973
Kirill Smelkov <kirr@nexedi.com>
2017-07-03 21:57:03 +0300

    .

--- go/xcommon/tracing/cmd/gotrace/gotrace_test.go
+++ go/xcommon/tracing/cmd/gotrace/gotrace_test.go
@@ -136,8 +136,7 @@ func TestGoTraceGen(t *testing.T) {
 	}
 
 	// XXX autodetect (go list ?)
-	//testv := []string{"a/pkg1", "b/pkg2", "c/pkg3", "d/pkg4"}
-	testv := []string{"a/pkg1"}
+	testv := []string{"a/pkg1", "b/pkg2", "c/pkg3", "d/pkg4"}
 
 	for _, tpkg := range testv {
 		err = tracegen(tpkg, tBuildCtx, "" /* = local imorts disabled */)
