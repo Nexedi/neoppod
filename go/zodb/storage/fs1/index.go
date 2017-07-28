39263985d582d96867d5ef6711c0d0851ca9222d
Kirill Smelkov <kirr@nexedi.com>
2017-07-28 13:25:35 +0300

    .

--- go/zodb/storage/fs1/index.go
+++ go/zodb/storage/fs1/index.go
@@ -18,7 +18,7 @@
 // See https://www.nexedi.com/licensing for rationale and options.
 
 package fs1
-// FileStorage v1. Index
+// index for quickly finding oid -> latest data record
 
 import (
 	"bytes"
