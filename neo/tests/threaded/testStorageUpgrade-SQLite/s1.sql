933579f5f77697ff0ebcdd62ab4afdb3d999d399
Julien Muchembled <jm@nexedi.com>
2018-05-22 20:26:07 +0200

    qa: original data for the future testStorageUpgrade
    These dumps were generated with an old version of NEO, plus a backport of the
    test that will use them.
    
    In MySQL dumps, --hex-blob was used only for inserts in the 'data' table.

---
+++ neo/tests/threaded/testStorageUpgrade-SQLite/s1.sql
@@ -0,0 +1,73 @@
+BEGIN TRANSACTION;
+CREATE TABLE config (
+                 name TEXT NOT NULL PRIMARY KEY,
+                 value TEXT);
+INSERT INTO "config" VALUES('name','testStorageUpgrade');
+INSERT INTO "config" VALUES('nid','1');
+INSERT INTO "config" VALUES('partitions','3');
+INSERT INTO "config" VALUES('replicas','1');
+INSERT INTO "config" VALUES('ptid','9');
+CREATE TABLE data (
+                 id INTEGER PRIMARY KEY AUTOINCREMENT,
+                 hash BLOB NOT NULL,
+                 compression INTEGER NOT NULL,
+                 value BLOB NOT NULL);
+INSERT INTO "data" VALUES(1,X'0BEEC7B5EA3F0FDBC95D0DD47F3C5BC275DA8A33',0,X'666F6F');
+CREATE TABLE obj (
+                 partition INTEGER NOT NULL,
+                 oid INTEGER NOT NULL,
+                 tid INTEGER NOT NULL,
+                 data_id INTEGER,
+                 value_tid INTEGER,
+                 PRIMARY KEY (partition, tid, oid));
+INSERT INTO "obj" VALUES(0,0,231616946283203125,1,NULL);
+CREATE TABLE pt (
+                 rid INTEGER NOT NULL,
+                 nid INTEGER NOT NULL,
+                 state INTEGER NOT NULL,
+                 PRIMARY KEY (rid, nid));
+INSERT INTO "pt" VALUES(0,1,0);
+INSERT INTO "pt" VALUES(1,1,0);
+INSERT INTO "pt" VALUES(0,2,0);
+INSERT INTO "pt" VALUES(2,2,0);
+INSERT INTO "pt" VALUES(1,3,1);
+INSERT INTO "pt" VALUES(2,3,1);
+DELETE FROM "sqlite_sequence";
+INSERT INTO "sqlite_sequence" VALUES('data',1);
+CREATE TABLE tobj (
+                 partition INTEGER NOT NULL,
+                 oid INTEGER NOT NULL,
+                 tid INTEGER NOT NULL,
+                 data_id INTEGER,
+                 value_tid INTEGER,
+                 PRIMARY KEY (tid, oid));
+CREATE TABLE trans (
+                 partition INTEGER NOT NULL,
+                 tid INTEGER NOT NULL,
+                 packed BOOLEAN NOT NULL,
+                 oids BLOB NOT NULL,
+                 user BLOB NOT NULL,
+                 description BLOB NOT NULL,
+                 ext BLOB NOT NULL,
+                 ttid INTEGER NOT NULL,
+                 PRIMARY KEY (partition, tid));
+INSERT INTO "trans" VALUES(1,231616946283203125,0,X'0000000000000000',X'',X'',X'286470310A2E',231616946283203125);
+CREATE TABLE ttrans (
+                 partition INTEGER NOT NULL,
+                 tid INTEGER NOT NULL,
+                 packed BOOLEAN NOT NULL,
+                 oids BLOB NOT NULL,
+                 user BLOB NOT NULL,
+                 description BLOB NOT NULL,
+                 ext BLOB NOT NULL,
+                 ttid INTEGER NOT NULL);
+CREATE INDEX _obj_i1 ON
+                 obj(partition, oid, tid)
+          ;
+CREATE INDEX _obj_i2 ON
+                 obj(data_id)
+          ;
+CREATE UNIQUE INDEX _data_i1 ON
+                 data(hash, compression)
+          ;
+COMMIT;
