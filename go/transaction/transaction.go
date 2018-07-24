dc78282ead2423ffc541cf01bc0118ed1b7b7c6a
Kirill Smelkov <kirr@nexedi.com>
2018-07-24 15:26:19 +0300

    .

--- go/transaction/transaction.go
+++ go/transaction/transaction.go
@@ -13,4 +13,84 @@
 
 package transaction
 
+import (
+	"context"
+	"sync"
+)
 
+// transaction implements Transaction.
+type transaction struct {
+	mu	sync.Mutex
+	status	Status
+	datav	[]DataManager
+	syncv	[]Synchronizer
+
+	// metadata
+	user	    string
+	description string
+	extension   string // XXX
+}
+
+// ctxKey is type private to transaction package, used as key in contexts.
+type ctxKey struct{}
+
+// getTxn returns transaction associated with provided context.
+// nil is returned is there is no association.
+func getTxn(ctx context.Context) *transaction {
+	t := ctx.Value(ctxKey{})
+	if t == nil {
+		return nil
+	}
+	return t.(*transaction)
+}
+
+// currentTxn serves Current.
+func currentTxn(ctx context.Context) Transaction {
+	txn := getTxn(ctx)
+	if txn == nil {
+		panic("transaction: no current transaction")
+	}
+	return txn
+}
+
+// newTxn serves New.
+func newTxn(ctx context.Context) (Transaction, context.Context) {
+	if getTxn(ctx) != nil {
+		panic("transaction: nested transactions are not supported")
+	}
+
+	txn := &transaction{status: Active}
+	txnCtx := context.WithValue(ctx, ctxKey{}, txn)
+	return txn, txnCtx
+}
+
+
+func (txn *transaction) Status() Status {
+	txn.mu.Lock()
+	defer txn.mu.Unlock()
+	return txn.status
+}
+
+func (txn *transaction) Commit(ctx context.Context) error {
+	panic("TODO")
+}
+
+func (txn *transaction) Abort() {
+	panic("TODO")
+}
+
+
+func (txn *transaction) Join(dm DataManager) {
+	panic("TODO")
+}
+
+func (txn *transaction) RegisterSync(sync Synchronizer) {
+	panic("TODO")
+}
+
+
+// ---- meta ----
+
+func (txn *transaction) User() string		{ return txn.user		}
+func (txn *transaction) Description() string	{ return txn.description	}
+func (txn *transaction) Extension() string	{ return txn.extension		}
