93b98fd2e6ff51500744372b58a06508f8d73d3c
Kirill Smelkov <kirr@nexedi.com>
2018-08-07 14:53:09 +0300

    .

--- go/zodb/db_test.go
+++ go/zodb/db_test.go
@@ -1,15 +1,21 @@
-// Copyright (c) 2001, 2002 Zope Foundation and Contributors.
-// All Rights Reserved.
-//
 // Copyright (C) 2018  Nexedi SA and Contributors.
 //                     Kirill Smelkov <kirr@nexedi.com>
 //
-// This software is subject to the provisions of the Zope Public License,
-// Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
-// THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
-// WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-// WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
-// FOR A PARTICULAR PURPOSE
+// This program is free software: you can Use, Study, Modify and Redistribute
+// it under the terms of the GNU General Public License version 3, or (at your
+// option) any later version, as published by the Free Software Foundation.
+//
+// You can also Link and Combine this program with other software covered by
+// the terms of any of the Free Software licenses or any of the Open Source
+// Initiative approved licenses and Convey the resulting work. Corresponding
+// source of such a combination shall include the source code for all other
+// software used.
+//
+// This program is distributed WITHOUT ANY WARRANTY; without even the implied
+// warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+//
+// See COPYING file for full licensing terms.
+// See https://www.nexedi.com/licensing for rationale and options.
 
 package zodb
 
