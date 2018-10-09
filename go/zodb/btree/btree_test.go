2b8a5115b1bc653c2bc91c9d2e87eeb0c4d93eb8
Kirill Smelkov <kirr@nexedi.com>
2018-10-09 22:22:22 +0300

    .

--- go/zodb/btree/btree_test.go
+++ go/zodb/btree/btree_test.go
@@ -78,7 +78,6 @@ func (b *bucketWrap) withBucket(ctx context.Context, f func()) error {
 
 func (b *bucketWrap) Get(ctx context.Context, key int64) (v interface{}, ok bool, err error) {
 	err = b.withBucket(ctx, func() {
-		//v, ok = b.LOBucket.get(key)	// XXX -> Get
 		v, ok = b.bucket().get(key)	// XXX -> Get
 	})
 	return
