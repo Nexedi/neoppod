a4667886e37eaf763af78c8419742567de5cc94b
Julien Muchembled <jm@nexedi.com>
2025-03-26 18:13:10 +0100

    Update documentation of issues with the feature to check replicas

--- neo/storage/checker.py
+++ neo/storage/checker.py
@@ -14,6 +14,40 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+"""
+WARNING: Experimental feature.
+
+BUG: The current implementation is not aware of differences
+     that can happen during pack operations.
+
+BUG: A backup cell may be wrongly marked as corrupted while checking replicas.
+
+    This happens in the following conditions:
+
+    1. a backup cluster starts to check replicas whereas a cell is outdated
+    2. this cell becomes updated, but only up to a tid smaller than
+       the max tid to check (this can't happen for a non-backup cluster)
+    3. the cluster actually starts to check the related partition
+    4. the cell is checked completely before it could replicate up to
+       the max tid to check
+
+    Sometimes, it causes the master to crash:
+
+        File "neo/lib/handler.py", line 72, in dispatch
+          method(conn, *args, **kw)
+        File "neo/master/handlers/storage.py", line 93, in notifyReplicationDone
+          cell_list = app.backup_app.notifyReplicationDone(node, offset, tid)
+        File "neo/master/backup_app.py", line 337, in notifyReplicationDone
+          assert cell.isReadable()
+      AssertionError
+
+    Workaround: make sure all cells are up-to-date before checking replicas.
+
+    Found by running testBackupNodeLost many times:
+    - either a failureException: 12 != 11
+    - or the above assert failure, in which case the unit test freezes
+"""
+
 from collections import deque
 from neo.lib import logging
 from neo.lib.connection import ClientConnection, ConnectionClosed
